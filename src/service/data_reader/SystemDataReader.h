778f599c7d785e41f3229bcdf0ee36a060296b86
DNadas98 <daniel.nadas.123@gmail.com>
2024-05-15 11:47:44 +0200

    refactor SystemDataReader

--- src/service/data_reader/SystemDataReader.h
+++ src/service/data_reader/SystemDataReader.h
@@ -12,4 +12,16 @@ public:
     static std::unique_ptr<SystemDataReader> create();
 };
 
+#ifdef __linux__
+#include "LinuxSystemDataReader.h"
+#endif
+
+std::unique_ptr<SystemDataReader> SystemDataReader::create() {
+#ifdef __linux__
+  return std::make_unique<LinuxSystemDataReader>();
+#else
+  throw std::runtime_error("Platform support not yet implemented");
+#endif
+}
+
 #endif // SYSTEM_DATA_READER_H
